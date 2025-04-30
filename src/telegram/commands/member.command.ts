5ad1b375744251fbe39dc30049b3d333d1ef9c3e
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-04-30 16:42:13 +0500

    -up member.command

--- src/telegram/commands/member.command.ts
+++ src/telegram/commands/member.command.ts
@@ -21,14 +21,21 @@ const helpJson = JSON.stringify(<IMember>{
   tag: "???"
 }, undefined, 2);
 
+const actions = ["add", "update", "delete"];
+
 const help =
   "Учтите, Вы должны передавать участник в типе JSON на следующей строке.\n"
-  + "Например:\n/addMember\n"
-  + helpJson;
+  + "Например:\n/member add\n"
+  + helpJson
 
-export const sendHelp = (interaction: IInteraction) => {
-  return interaction.reply(help, { entities: [{
-    offset: help.length - helpJson.length,
+const addition =
+  "\n\nВсе возможные вариации команды:\n" + actions.map(c => "/member " + c).join("\n")
+  + "\n\nДля обновления пользователя введите: /member update {{ member name }}\n{{ JSON data }}"
+  + "\n\nДля удаления пользователя введите /member delete {{ member name }}";
+
+export const sendHelp = (interaction: IInteraction, prefix: string = "") => {
+  return interaction.reply((prefix + "\n") + help + addition, { entities: [{
+    offset: (prefix + "\n").length + help.length - helpJson.length,
     length: helpJson.length,
     type: "blockquote"
   }]});
@@ -37,7 +44,7 @@ export const sendHelp = (interaction: IInteraction) => {
 const parser = new Parser("members");
 
 class Command implements ICommand {
-  public readonly name = "addMember";
+  public readonly name = "member";
 
   public async execute(interaction: IInteraction) {
     if (!interaction.from) {
@@ -49,14 +56,40 @@ class Command implements ICommand {
     if (!interaction.text) {
       return interaction.reply("Текст взаимодействия не найден.")
     };
-    if (interaction.text.split("\n").length < 2) {
-      return sendHelp(interaction);
-    }
+    if (interaction.text.split("\n").length < 2 && interaction.text.split(" ")[1] !== "delete") {
+      return sendHelp(interaction, "Ошикба 3. JSON не найден.");
+    };
+    if (interaction.text.split("\n")[0].split(" ").length < 2) {
+      return sendHelp(interaction, "Ошибка 4. Действие не вписано.");
+    };
 
+    let action: string = interaction.text.split(" ")[1].split("\n")[0];
+    let name: string = "NONE";
+    
+    if (!actions.includes(action)) {
+      return sendHelp(interaction, "Ошибка 1. Действие не найдено.");
+    };
+    if (action !== "add") {
+      if (interaction.text.split(" ").length < 3) {
+        return sendHelp(interaction, "Ошибка 2. Имя не вписано.");
+      }
+
+      name = interaction.text.split(" ")[2].split("\n")[0];
+    };
+    
     try {
+      if (action === "delete") {
+        return interaction.reply(
+          "Действие выполнено.\n" + JSON.stringify(
+            await Members.deleteOne({ name: name }), undefined, 2
+        ));
+      };
+
       const json = {
         socials: [],
         description: "",
+        avatar: "",
+        meta: "",
         ...JSON.parse(interaction.text.split("\n").splice(1).join(""))
       } as IMember;
 
@@ -64,9 +97,11 @@ class Command implements ICommand {
         if (!(k in json)) throw new Error(`Ключ ${k} не найден в JSON`);
       });
 
-      const member = await Members.create(parser.execute(json));
-
-      return interaction.reply("Создан новый участник.\n" + JSON.stringify(parser.execute(member), undefined, 2));
+      return interaction.reply("Действие выполнено.\n" + JSON.stringify((
+        action === "add"
+          ? await Members.create(parser.execute(json))
+          : await Members.updateOne({ name: name }, json)
+      ), undefined, 2));
     } catch (error) {
       return interaction.reply("Произошла непредвиденная ошибка.\n" + error);
     }
