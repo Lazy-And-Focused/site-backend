11fcc60f8f4d7c74c33396add5588513a94a46bc
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-05-01 21:20:31 +0500

    -hot-fix

--- src/init.ts
+++ src/init.ts
@@ -9,21 +9,25 @@ export const initializeProjects = async () => {
   const members = await Members.find();
 
   for (const member of members) {
-    const { success, data: repos } = await service.getRepos(member.tag);
-    
-    console.log("Инициализирую проекты " + member.tag);
-
-    if (!success || !Array.isArray(repos)) return;
-    if (repos.length === 0) return;
-
-    const projects = await service.resolveRepos(repos);
-
-    for (const project of projects) {
-      try {
-        console.log("Инициализирую " + project.name);
-        const p = await Projects.create({ ...DEFAULT, ...project});
-        console.log(p.name + " инициализирован");
-      } catch {};
+    try {
+      const { success, data: repos } = await service.getRepos(member.tag);
+      
+      console.log("Инициализирую проекты " + member.tag);
+  
+      if (!success || !Array.isArray(repos)) return;
+      if (repos.length === 0) return;
+  
+      const projects = await service.resolveRepos(repos);
+  
+      for (const project of projects) {
+        try {
+          console.log("Инициализирую " + project.name);
+          const p = await Projects.create({ ...DEFAULT, ...project});
+          console.log(p.name + " инициализирован");
+        } catch {};
+      }
+    } catch (error) {
+      continue;      
     }
   };
 }
